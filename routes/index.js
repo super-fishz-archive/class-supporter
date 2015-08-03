2435f6c37a3c506863dc92055a65feabbfaaa0bf
super-fish <super_fishz@core-a.org>
2015-08-03 19:53:21 +0900

    db connection add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var model = require('../app/model');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    model.query("select now() as date from dual", function(err, rows) {
+        // console.log(rows);
+        res.render('index', { title: 'Express', db_time: rows[0].date});
+    });
 });
 
 module.exports = router;

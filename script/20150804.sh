57e395400a40779c42e508d8049f8e59ab44de09
super-fish <super_fishz@core-a.org>
2015-08-03 20:00:38 +0900

    today script add

---
+++ script/20150804.sh
@@ -0,0 +1,8 @@
+sudo apt-cache policy mysql-server
+sudo apt-get install mysql-client mysql-server
+sudo npm install -g nodemon
+redis-server --version
+cd /[project directory/class-supporter
+git pull
+npm install
+mysql -u root -p  < document/socrativeSQL.sql 
